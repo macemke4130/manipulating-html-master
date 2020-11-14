e326eb0ab20550bd7c57ab9efa5aabb1e9f8e493
Lucas Mace <lucasmace4130@gmail.com>
2020-11-13 21:48:01 -0600

    First few lines of DOM

---
+++ DOM/app.js
@@ -0,0 +1,7 @@
+
+let x = document.createElement('button');
+x.textContent = "MyButton";
+document.body.prepend(x);
+let step1 = document.createElement('h3');
+step1.textContent = "Step One";
+document.body.prepend(step1);
